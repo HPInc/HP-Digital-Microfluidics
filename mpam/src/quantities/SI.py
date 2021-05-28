0a65f9ab83e4d5b63860b3c5ee480190298bddfb
Evan Kirshenbaum <evan.kirshenbaum@hp.com>
2021-05-28 14:35:48 -0700

    Cleaned up units in quantities and added plurals for word variables.

--- mpam/src/quantities/SI.py
+++ mpam/src/quantities/SI.py
@@ -2,115 +2,140 @@ from quantities import dimensions, prefixes
 import math
 
 # angle
-rad = radian = dimensions.Angle(1).as_unit("rad")
-sr = steradian = dimensions.SolidAngle(1).as_unit("sr")
-deg = degree = (math.pi/180)*rad
+rad = radian = radians = dimensions.Angle(1).as_unit("rad")
+sr = steradian = steradians = dimensions.SolidAngle(1).as_unit("sr")
+deg = degree = degrees = (math.pi/180)*rad
 
 # mass
 g = gramme = gram = dimensions.Mass.base_unit("g")
+grammes = grams = g
 µg = ug = microgramme = microgram = prefixes.micro(gram)
+microgrammes = micrograms = ug
 mg = milligramme = milligram = prefixes.milli(gram)
+milligrammes = milligrams = mg
 kg = kilogramme = kilogram = prefixes.kilo(gram)
+kilogrammes = kilograms = kg
 tonne = metric_tonne = metric_ton = (1000*kg).as_unit("tonne")
+tonnes = metric_tonnes = metric_tons = tonne
 
 # distance
 m = metre = meter = dimensions.Distance.base_unit("m")
+metres = meters = m
 dm = decimetre = decimeter = prefixes.deci(meter)
+decimetres = decimeters = dm
 cm = centimetre = centimeter = prefixes.centi(meter)
+centimetres = centimeters = cm
 mm = millimetre = millimeter = prefixes.milli(meter)
+millimetres = millimeters = mm
 µm = um = micron = micrometre = micrometer = prefixes.micro(meter)
+microns = micrometres = micrometers = um
 nm = nanometre = nanometer = prefixes.nano(meter)
+nanometres = nanometers = nm
 km = kilometre = kilometer = prefixes.kilo(meter)
+kilometres = kilometers = km
 
 # area
-ha = hectare = (m**2).a(dimensions.Area).as_unit("ha")
+ha = hectares = hectare = (m**2).a(dimensions.Area).as_unit("ha")
 
 # volume
 l = L = litre = liter = (dm**3).a(dimensions.Volume).as_unit("l")
+litres = liters = L
 dl = dL = decilitre = deciliter = prefixes.deci(liter)
+decilitres = deciliters = dL
 cl = cL = centilitre = centiliter = prefixes.centi(liter)
+centilitres = centiliters = cL
 ml = mL = millilitre = milliliter = prefixes.milli(liter)
+millilitres = milliliters = mL
 µl = µL = ul = uL = micorlitre = microliter = prefixes.micro(liter)
-st = stere = (m**3).a(dimensions.Volume).as_unit("st")
+microlitres = microliters = uL
+st = steres = stere = (m**3).a(dimensions.Volume).as_unit("st")
 
 # time
 s = sec = second = dimensions.Time.base_unit("s")
+secs = seconds = s
 ms = msec = millisec = millisecond = prefixes.milli(second)
+millisecs = milliseconds = ms
 µs = us = µsec = usec = microsec = microsecond = prefixes.micro(second)
+µsecs = usecs = microsecs = microseconds = us
 ns = nsec = nanosec = nanosecond = prefixes.nano(second)
-mins = minute = (60*second).as_unit("min")
-hr = hour = (60*minute).as_unit("hr")
-day = (24*hour).as_unit("day")
+nsecs = nanosecs = nanoseconds = ns
+mins = minutes = minute = (60*second).as_unit("min")
+hr = hours = hour = (60*minute).as_unit("hr")
+days = day = (24*hour).as_unit("day")
+wk = weeks = week = (7*day).as_unit("wk")
 
 # frequency
 Hz = hertz = (1/s).a(dimensions.Frequency).as_unit("Hz")
 
 # force
-N = newton = (kg*m/s**2).a(dimensions.Force).as_unit("N")
+N = newtons = newton = (kg*m/s**2).a(dimensions.Force).as_unit("N")
 
 # work
-J = joule = (N*m).a(dimensions.Work).as_unit("J")
+J = joules = joule = (N*m).a(dimensions.Work).as_unit("J")
 
 # pressure
-Pa = pascal = (N/m**2).a(dimensions.Pressure).as_unit("Pa")
+Pa = pascals = pascal = (N/m**2).a(dimensions.Pressure).as_unit("Pa")
 
 # power
-W = watt = (J/s).a(dimensions.Power).as_unit("W")
+W = watts = watt = (J/s).a(dimensions.Power).as_unit("W")
 
 # temperature
-K = kelvin = dimensions.Temperature.base_unit("K")
-deg_K = kelvin
-deg_C = kelvin.as_unit("°C")
+K = kelvins = kelvin = dimensions.Temperature.base_unit("K")
+deg_K = degrees_K = degree_K = degrees_Kelvin = degree_Kelvin = kelvin
+deg_C = degrees_C = degree_C = kelvin.as_unit("°C")
+degrees_Celsius = degree_Celcius = deg_C
+degrees_centigrade = degree_centigrade = deg_C
+
 
 # luminous intensity
-cd = candela = dimensions.LumInt.base_unit("cd")
+cd = candelas = candela = dimensions.LumInt.base_unit("cd")
 
 # luminous flux
-lm = lumen = (cd*sr).a(dimensions.LumFlux).as_unit("lm")
+lm = lumens = lumen = (cd*sr).a(dimensions.LumFlux).as_unit("lm")
 
 # illuminance
 lx = lux = (lm/m**2).a(dimensions.Illuminance).as_unit("lux")
 
 # current
-A = ampere = dimensions.Current.base_unit("A")
+A = amperes = ampere = dimensions.Current.base_unit("A")
 
 # charge
-C = coulomb = (A*s).a(dimensions.Charge).as_unit("C")
-faraday = (96485.33212310084*C).as_unit("F")
+C = coulombs = coulomb = (A*s).a(dimensions.Charge).as_unit("C")
+faradays = faraday = (96485.33212310084*C).as_unit("F")
 
 # voltage, electric potential, emf
-V = volt = (J/C).a(dimensions.Voltage).as_unit("V")
+V = volts = volt = (J/C).a(dimensions.Voltage).as_unit("V")
 
 # magnetic flux
-Wb = weber = (V*s).a(dimensions.MagneticFlux).as_unit("Wb")
+Wb = webers = weber = (V*s).a(dimensions.MagneticFlux).as_unit("Wb")
 
 # magnetic induction, flux density
-T = tesla = (Wb/m**2).a(dimensions.MagneticInduction).as_unit("T")
+T = teslas = tesla = (Wb/m**2).a(dimensions.MagneticInduction).as_unit("T")
 
 # capacitance
-F = farad = (C/V).a(dimensions.Capacitance).as_unit("F")
+F = farads = farad = (C/V).a(dimensions.Capacitance).as_unit("F")
 
 # resistance
-Ω = ohm = (V/A).a(dimensions.Resistance).as_unit("Ω")
+Ω = ohms = ohm = (V/A).a(dimensions.Resistance).as_unit("Ω")
 
 # conductance
 S = siemens = (A/V).a(dimensions.Conductance).as_unit("S")
-mho = S.as_unit("mho")
+mhos = mho = S.as_unit("mho")
 
 # inductance
-H = henry = (V*s/A).a(dimensions.Inductance).as_unit("H")
+H = henrys = henry = (V*s/A).a(dimensions.Inductance).as_unit("H")
 
 # radioactivity
-Bq = becquerel = (1/s).a(dimensions.Radioactivity).as_unit("Bq")
+Bq = becquerels = becquerel = (1/s).a(dimensions.Radioactivity).as_unit("Bq")
 
 # ionizing radiation dose
-Gy = gray = (J/kg).a(dimensions.IonizingRadDose).as_unit("Gy")
-Sv = sievert = (J/kg).a(dimensions.IonizingRadDose).as_unit("Sv")
+Gy = grays = gray = (J/kg).a(dimensions.IonizingRadDose).as_unit("Gy")
+Sv = sieverts = sievert = (J/kg).a(dimensions.IonizingRadDose).as_unit("Sv")
 
 # amount
-mol = mole = dimensions.Amount.base_unit("mol")
-mmol = millimole = prefixes.milli(mole)
-µmol = umol = micromole = prefixes.micro(mole)
+mol = moles = mole = dimensions.Amount.base_unit("mol")
+mmol = millimoles = millimole = prefixes.milli(mole)
+µmol = umol = micromoles = micromole = prefixes.micro(mole)
 
 
 # molar concentration
--- mpam/src/quantities/US.py
+++ mpam/src/quantities/US.py
@@ -2,91 +2,91 @@ from quantities import prefixes, SI, dimensions
 
 
 # mass
-lb = pound = (0.45359237*SI.kg).as_unit("lb")
-oz = ounce = (lb/16).as_unit("oz")
-dr = dram = (oz/16).as_unit("dr")
-gr = grain = (lb/7000).as_unit("gr")
-ct = carat = (200*SI.mg).as_unit("ct")
-cwt = hundredweight = (100*lb).as_unit("cwt")
-ton = (2000*lb).as_unit("ton")
-long_cwt = long_hundredweight = (112*lb).as_unit("long_cwt")
-long_ton = (2240*lb).as_unit("long_ton")
+lb = pounds = pound = (0.45359237*SI.kg).as_unit("lb")
+oz = ounces = ounce = (lb/16).as_unit("oz")
+dr = drams = dram = (oz/16).as_unit("dr")
+gr = grains = grain = (lb/7000).as_unit("gr")
+ct = carats = carat = (200*SI.mg).as_unit("ct")
+cwt = hundredweights = hundredweight = (100*lb).as_unit("cwt")
+tons = ton = (2000*lb).as_unit("ton")
+long_cwt = long_hundredweights = long_hundredweight = (112*lb).as_unit("long_cwt")
+long_tons = long_ton = (2240*lb).as_unit("long_ton")
 
-dwt = pennyweight = (24*gr).as_unit("dwt")
-oz_t = troy_oz = troy_ounce = (20*dwt).as_unit("oz_t")
-lb_t = troy_lb = troy_pound = (12*oz_t).as_unit("lb_t")
+dwt = pennyweights = pennyweight = (24*gr).as_unit("dwt")
+oz_t = troy_oz = troy_ounces = troy_ounce = (20*dwt).as_unit("oz_t")
+lb_t = troy_lb = troy_pounds = troy_pound = (12*oz_t).as_unit("lb_t")
 
-slug = (32.1740*lb).as_unit("slug")
+slugs = slug = (32.1740*lb).as_unit("slug")
 
 # distance 
 
-inch = (2.54*SI.cm).as_unit("in")
-ft = foot = (12*inch).as_unit("ft")
-yd = yard = (3*ft).as_unit("yd")
-mi = mile = (5280*ft).as_unit("mi")
-point = (inch/72).as_unit("point")
-fur = furlong = (660*ft).as_unit("fur")
-lea = league = (3*mi).as_unit("lea")
-ftm = fathom = (2*yd).as_unit("yd")
-nmi = nautical_mile = (1.151*mi).as_unit("nmi")
-hand = (4*inch).as_unit("hand")
-th = thou = (inch/1000).as_unit("th")
-ch = chain = (66*ft).as_unit("ch")
-cable = (100*ftm).as_unit("cable")
+inches = inch = (2.54*SI.cm).as_unit("in")
+ft = feet = foot = (12*inch).as_unit("ft")
+yd = yards = yard = (3*ft).as_unit("yd")
+mi = miles = mile = (5280*ft).as_unit("mi")
+points = point = (inch/72).as_unit("point")
+fur = furlongs = furlong = (660*ft).as_unit("fur")
+lea = leagues = league = (3*mi).as_unit("lea")
+ftm = fathoms = fathom = (2*yd).as_unit("yd")
+nmi = nautical_miles = nautical_mile = (1.151*mi).as_unit("nmi")
+hands = hand = (4*inch).as_unit("hand")
+th = thous = thou = (inch/1000).as_unit("th")
+ch = chains = chain = (66*ft).as_unit("ch")
+cables = cable = (100*ftm).as_unit("cable")
 
 #area
 
-sq_in = square_inch = (inch**2).a(dimensions.Area).as_unit("sq_in")
-sq_ft = square_foot = (ft**2).a(dimensions.Area).as_unit("sq_ft")
-sq_mi = square_mile = (mi**2).a(dimensions.Area).as_unit("sq_mi")
-acre = (sq_mi/640).as_unit("acre")
-section = (sq_mi).as_unit("section")
+sq_in = square_inches = square_inch = (inch**2).a(dimensions.Area).as_unit("sq_in")
+sq_ft = square_feet = square_foot = (ft**2).a(dimensions.Area).as_unit("sq_ft")
+sq_mi = square_miles = square_mile = (mi**2).a(dimensions.Area).as_unit("sq_mi")
+acres = acre = (sq_mi/640).as_unit("acre")
+sections = section = (sq_mi).as_unit("section")
 
 # volume
-cu_in = cubic_inch = (inch**3).a(dimensions.Volume).as_unit("cu_in")
-gal = gallon = (231*cu_in).as_unit("gal")
-fl_oz = fluid_ounce = (gal/128).as_unit("fl_oz")
-qt = quart = (32*fl_oz).as_unit("qt")
-pt = pint = (16*fl_oz).as_unit("pt")
-cup = (8*fl_oz).as_unit("C")
-Tbsp = tablespoon = (fl_oz/2).as_unit("Tbsp")
-tsp = teaspoon = (Tbsp/3).as_unit("tsp")
-fl_dr = fluid_dram = (Tbsp/4).as_unit("fl_dr")
-minim = (tsp/80).as_unit("min")
-shot = jig = jigger = (1.5*fl_oz).as_unit("jig")
-bbl = barrel = (31.5*gal).as_unit("bbl")
-hogshead = (63*gal).as_unit("hogshead")
-board_ft = board_foot = (ft**2*inch).a(dimensions.Volume).as_unit("board_ft")
+cu_in = cubic_inches = cubic_inch = (inch**3).a(dimensions.Volume).as_unit("cu_in")
+gal = gallons = gallon = (231*cu_in).as_unit("gal")
+fl_oz = fluid_ounces = fluid_ounce = (gal/128).as_unit("fl_oz")
+qt = quarts = quart = (32*fl_oz).as_unit("qt")
+pt = pints = pint = (16*fl_oz).as_unit("pt")
+cups = cup = (8*fl_oz).as_unit("C")
+Tbsp = tablespoons = tablespoon = (fl_oz/2).as_unit("Tbsp")
+tsp = teaspoons = teaspoon = (Tbsp/3).as_unit("tsp")
+fl_dr = fluid_drams = fluid_dram = (Tbsp/4).as_unit("fl_dr")
+minims = minim = (tsp/80).as_unit("min")
+shots = shot = jig = jiggers = jigger = (1.5*fl_oz).as_unit("jig")
+bbl = barrels = barrel = (31.5*gal).as_unit("bbl")
+hogsheads = hogshead = (63*gal).as_unit("hogshead")
+board_ft = board_feet = board_foot = (ft**2*inch).a(dimensions.Volume).as_unit("board_ft")
 
-pt_d = dry_pt = dry_pint = (33.6003125*cu_in).as_unit("pt_d")
-qt_d = dry_qt = dry_quart = (2*pt_d).as_unit("qt_d")
-gal_d = dry_gal = dry_gallon = (4*qt_d).as_unit("gal_d")
-pk = peck = (2*gal_d).as_unit("pk")
-bu = bushel = (4*pk).as_unit("bu")
-bbl_d = dry_bbl = dry_barrel = (7056*cu_in).as_unit("bbl_d")
+pt_d = dry_pt = dry_pints = dry_pint = (33.6003125*cu_in).as_unit("pt_d")
+qt_d = dry_qt = dry_quarts = dry_quart = (2*pt_d).as_unit("qt_d")
+gal_d = dry_gal = dry_gallons = dry_gallon = (4*qt_d).as_unit("gal_d")
+pk = pecks = peck = (2*gal_d).as_unit("pk")
+bu = bushels = bushel = (4*pk).as_unit("bu")
+bbl_d = dry_bbl = dry_barrels = dry_barrel = (7056*cu_in).as_unit("bbl_d")
 
 # time
 
-s = SI.s
-mins = SI.mins
-hr = SI.hr
+from quantities.SI import s,sec,second,secs,seconds,mins,minutes,minute,hr,hours,hour,days,day,wk,weeks  # @UnusedImport
+
 
 # velocity
-fps = foot_per_second = (ft/s).a(dimensions.Velocity).as_unit("fps")
-mph = mile_per_hour = (mi/hr).a(dimensions.Velocity).as_unit("mph")
+fps = feet_per_second = foot_per_second = (ft/s).a(dimensions.Velocity).as_unit("fps")
+mph = miles_per_hour = mile_per_hour = (mi/hr).a(dimensions.Velocity).as_unit("mph")
 
 # temperature
-deg_F = (1.8*SI.deg_C).as_unit("°F")
+deg_F = degrees_F = degree_F = (1.8*SI.deg_C).as_unit("°F")
+degrees_Fahrenheit = degree_Fahrenheit = deg_F
 
 # work
-Btu = (1054*prefixes.kilo(SI.J)).as_unit("Btu")
-cal = small_cal = small_calorie = (4.184*SI.J).as_unit("cal")
-kcal = kilocalorie = prefixes.kilo(small_cal)
-Cal = food_calorie = calorie = (kcal).as_unit("Cal")
+Btu = BTU = BTUs = Btus = (1054*prefixes.kilo(SI.J)).as_unit("Btu")
+cal = small_cal = small_calories = small_calorie = (4.184*SI.J).as_unit("cal")
+kcal = kilocalories = kilocalorie = prefixes.kilo(small_cal)
+Cal = food_calories = food_calorie = calories = calorie = (kcal).as_unit("Cal")
 
 # force
-pdl = poundal = (lb*ft/s**2).a(dimensions.Force).as_unit("pdl")
-lbf = pound_force = (slug*ft/s**2).a(dimensions.Force).as_unit("lbf")
+pdl = poundals = poundal = (lb*ft/s**2).a(dimensions.Force).as_unit("pdl")
+lbf = pounds_force = pound_force = (slug*ft/s**2).a(dimensions.Force).as_unit("lbf")
 
 # pressure
 psi = (lbf/sq_in).a(dimensions.Pressure).as_unit("psi")
