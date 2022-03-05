cb102a849852e4610a33f65307179f6062d82022
Evan Kirshenbaum <evan.kirshenbaum@hp.com>
2022-03-04 16:29:43 -0800

    issue #67. Can talk to Glider directly.

--- mpam/tests/scratch.py
+++ mpam/tests/scratch.py
@@ -1,14 +1,35 @@
 from __future__ import annotations
 
-from quantities.SI import mL
-from quantities.dimensions import Volume
-from quantities.US import gal
+import pyglider
+from pathlib import Path
 
-v = 3*gal
-print(Volume.default_units)
-print(f"{v:,g}")
 
-Volume.default_units = mL
-print(Volume.default_units)
-print(f"{v:,g}")
+wallaby = pyglider.Board.Find(pyglider.BoardId.Wallaby,
+                              dll_dir = Path("d:\\dmf-git\\DynamicHAL\\x64\\Debug"),
+                              config_dir = Path("d:\\dmf-git\\DynamicHAL\\WallabyHAL"))
+print(wallaby)
+print((wallaby.GetBoardRows(), wallaby.GetBoardColumns()))
+
+es = wallaby.GetElectrodes()
+print(len(es))
+print(es[0])
+# print(pyglider.test())
+
+print([e.GetCoordinates() for e in es[0:5]])
+print([e.GetCurrentState() for e in es[0:5]])
+print([e.GetName() for e in es[130:132]])
+print([e.GetName() for e in es[130:132]])
+# print(wallaby.GetHeaters()[0].GetName())
+# print(es[0].GetCoordinates())
+
+# print(wallaby.GetElectrodes())
+# print(wallaby.GetElectrodes()[0].GetName())
+# wallaby: pyglider.Board
+
+
+# import pyglider;
+# pyglider.Bo
+# # wallaby = pyglider.Board.Find(pyglider.BoardId.Wallaby, "d:\\dmf-git\\DynamicHAL\\x64\\Debug")
+# wallaby = pyglider.Board.Find(pyglider.BoardId.Wallaby, "..\\local\\glider")
+
 
