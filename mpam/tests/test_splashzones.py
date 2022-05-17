10a8998b1d5636976ca8e70c0cee82aae27126cb
Rares Vernica <rares.vernica@hp.com>
2022-05-16 20:45:01 -0700

    Add drops looping around extraction point while other drops move

--- mpam/tests/test_splashzones.py
+++ mpam/tests/test_splashzones.py
@@ -1,23 +1,38 @@
+import argparse
 import logging
 
 from devices import joey, dummy_pipettor
 from mpam.device import System
 from mpam.paths import Path
 from mpam.types import Reagent
-from quantities.SI import ms
+from quantities.SI import ms, s
+
 
 
 def test_splashzones(system: System) -> None:
-    ep = system.board.extraction_points[0]
-    p1 = Path.teleport_into(ep, reagent=Reagent('R1')).to_pad((11, 15))
-    p2 = Path.teleport_into(ep, reagent=Reagent('R2')).to_pad((11, 13))
-    p3 = Path.teleport_into(ep, reagent=Reagent('R3')).to_pad((11, 17))
+    ep= system.board.extraction_points[0]
+    loop = ((11, 17), (11, 13), (15, 13), (15, 17))
+
+    paths_loop = []
+    for i in range(3):
+        path = Path.teleport_into(ep, reagent=Reagent(f'R{i}'))
+        seq = loop * 3
+        if i:
+            seq = seq[:-i]
+        for pos in seq:
+            path = path.to_pad(pos, after=1 * s)
+        paths_loop.append(path)
+
+    paths_waste = []
+    for i in range(10):
+        paths_waste.append(Path.teleport_into(ep, reagent=Reagent('R')).to_pad((0, 18)).enter_well())
 
     system.clock.start(200 * ms)
+    # system.clock.update_interval = 200 * ms
+
     with system.batched():
-        p1.schedule()
-        p2.schedule()
-        p3.schedule()
+        for p in paths_loop + paths_waste:
+            p.schedule()
 
 
 logging.basicConfig(level=logging.DEBUG,
@@ -33,6 +48,8 @@ system = System(
             short_transit_time=1 * ms,
             long_transit_time=1 * ms,
             get_tip_time=1 * ms,
-            drop_tip_time=1 * ms)))
-system.run_monitored(test_splashzones, min_time=0 * ms)
+            drop_tip_time=1 * ms),
+        extraction_point_splash_radius=2))
+args = argparse.Namespace(highlight_reservations=True)
+system.run_monitored(test_splashzones, min_time=30 * s, cmd_line_args=args)
 system.stop()
