c61324ca7bd299c7cc83f28e269c9d66356dc436
Evan Kirshenbaum <evan.kirshenbaum@hp.com>
2022-06-07 13:29:15 -0700

    issue #162. Generalized --log-level to specify format and configure loggers for imported modules.

--- mpam/tests/scratch.py
+++ mpam/tests/scratch.py
@@ -1,34 +1,3 @@
 from __future__ import annotations
 
-from devices import joey
-from quantities.SI import volts, V
-from quantities.dimensions import Voltage
-from mpam.types import OnOff
-from mpam.device import PowerMode
-
-
-Voltage.default_units = volts
-
-board = joey.Board(ps_can_toggle=False, ps_initial_voltage=30*V)
-ps = board.power_supply
-
-print(ps)
-
-ps.mode = PowerMode.AC
-ps.voltage = 20*V
-print(ps)
-
-ps.voltage = 80*V
-print(ps)
-
-ps.voltage = 600*V
-print(ps)
-      
-ps.current_state = OnOff.OFF
-print(ps)
-
-ps.current_state = OnOff.ON
-print(ps)
-
-ps.voltage = 0*V
-print(ps)
+print(int("foo"))
