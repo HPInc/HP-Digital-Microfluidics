fd4361f3a8106dca29e6772cbe300f6c2806210d
Evan Kirshenbaum <evan.kirshenbaum@hp.com>
2022-09-13 14:53:57 -0700

    issue #198.  Added OpenDropFirmware class to abstract out communication with OpenDrop hardware for wombat and opendrop devices.

--- mpam/src/devices/opendrop.py
+++ mpam/src/devices/opendrop.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from argparse import Namespace, _ArgumentGroup, ArgumentParser
-from typing import Optional, Final, Sequence
+from argparse import Namespace, _ArgumentGroup, ArgumentParser,\
+    BooleanOptionalAction
+from typing import Optional, Final, Sequence, Callable
 
 from serial import Serial
 
@@ -15,25 +16,150 @@ from mpam.pipettor import Pipettor
 from mpam.types import OnOff, XYCoord, Orientation, Dir, State
 from quantities.SI import uL, ms
 from quantities.dimensions import Time
+from abc import abstractmethod
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Electrode(State[OnOff]):
-    index: Final[int]
+    byte_index: Final[int]
+    bit_index: Final[int]
+
     array: Final[bytearray]
     
     def realize_state(self, val: OnOff) -> None:
-        self.array[self.index] = 1 if val else 0
-    
-    def __init__(self, x: int, y: int, a: bytearray) -> None:
-        super().__init__(initial_state=OnOff.OFF)
-        self.index = x*8+y
-        self.array = a
+        # assert self.index < len(self.array), f"{self.index} not in {len(self.array)}-byte array"
         
+        bit = 1 << self.bit_index
+
+        if val is OnOff.ON:
+            self.array[self.byte_index] |= bit
+        else:
+            self.array[self.byte_index] &= ~bit
+        
+        # self.array[self.index] = 1 if val else 0
+    
+    def __init__(self, byte_index: int, bit_index: int, a: bytearray) -> None:
+        super().__init__(initial_state=OnOff.OFF)
+        self.byte_index = byte_index
+        self.bit_index = bit_index
+        self.array = a
+
+
+class OpenDropFirmware:
+    _states: Final[bytearray]
+    _last_states: bytearray
+    _device: Final[Optional[str]]
+    _port: Optional[Serial] = None
+    _double_write: Final[bool]
+    
+    def __init__(self, args: Namespace) -> None:
+        self._states = bytearray(self.n_state_bytes)
+        self._last_states = bytearray(self.n_state_bytes)
+        self._device = args.port
+        self._double_write = args.double_write
+    
+    @abstractmethod
+    def indices(self, x: int, y: int) -> tuple[int, int]: # @UnusedVariable
+        """
+        Returns the byte and bit indices
+        """
+        ...
+    @property
+    @abstractmethod
+    def n_state_bytes(self) -> int: ...
+    
+    def make_electrode(self, x: int, y: int) -> Electrode:
+        (byte_index, bit_index) = self.indices(x,y)
+        return Electrode(byte_index, bit_index, self._states)
+
+    @classmethod    
+    def add_args_to(cls, 
+                    group: _ArgumentGroup, 
+                    parser: ArgumentParser) -> None:
+        group.add_argument('-p', '--port',
+                           help='''
+                           The communication port (e.g., COM5) to use to talk to the board.
+                           By default, only the display is run
+                           ''')
+        vg = group.add_mutually_exclusive_group()
+        vg.add_argument('-4.0', action='store_const', const=OpenDropV40, dest='od_version',
+                        help="The OpenDrop board uses firmware version 4.0")
+        vg.add_argument('-4.1', action='store_const', const=OpenDropV41, dest='od_version',
+                        help="The OpenDrop board uses firmware version 4.1")
+        # group.add_argument('--yaminon', action='store_true',
+        #                    help="Mirror pads on top and bottom of the board.")
+        double_write_default = True
+        group.add_argument('--double-write', action=BooleanOptionalAction, default=double_write_default,
+                           help=f'''
+                           Send state array to OpenDrop twice.  
+                           Default is {double_write_default}
+                           ''')
+        parser.set_defaults(od_version=OpenDropV40)
+        
+    @classmethod
+    def make_firmware(cls, args: Namespace) -> OpenDropFirmware:
+        t: Callable[[Namespace], OpenDropFirmware] = args.od_version
+        return t(args)
+    
+    def send_states(self, states: bytearray) -> None:
+        logger.debug("sending %s", states.hex())
+        if self._port is not None:
+            self._port.write(states)
+            if self._double_write:
+                # I'm not sure why, but it seems that nothing happens until the 
+                # first byte of the next round gets sent. (Sending 129 bytes works, 
+                # but then the next round will use that extra byte.  Sending everything
+                # twice seems to do the job.  I'll look into this further.
+                self._port.write(states)
+    
+    def update_state(self, delay: Time) -> None:
+        if self._port is None and self._device is not None:
+            self._port = Serial(self._device)
+        new_states = bytearray(self._states)
+        send_first: Optional[bytearray] = None
+        if delay > 0:
+            # First we send 
+            send_first = bytearray(x&y for x,y in zip(self._last_states, new_states))
+        elif delay < 0:
+            send_first = bytearray(x|y for x,y in zip(self._last_states, new_states))
+            delay = -delay
+        if send_first is not None:
+            self.send_states(send_first)
+            logger.debug("Delaying for %s", delay)
+            delay.sleep()
+        self.send_states(new_states)
+        self._last_states = new_states
+    
+    def stop(self)->None:
+        if self._port is not None:
+            self._port.close()
+            self._port = None
+
+    
+class OpenDropV40(OpenDropFirmware):
+    @property
+    def n_state_bytes(self) -> int:
+        return 128
+    def indices(self, x:int, y:int)->tuple[int, int]:
+        byte_index = x*8+y
+        bit_index = 0
+        assert byte_index < 128
+        return (byte_index, bit_index)
+
+class OpenDropV41(OpenDropFirmware):
+    @property
+    def n_state_bytes(self) -> int:
+        return 32
+    def indices(self, x:int, y:int)->tuple[int, int]:
+        byte_index = x
+        bit_index = y
+        return (byte_index, bit_index)
+
+
 
 class Board(device.Board):
-    _dev: Optional[str]
-    _states: bytearray
-    _port: Optional[Serial]
+    _firmware: Final[OpenDropFirmware]
     
     def _long_pad_bounds(self, ep_loc: XYCoord) -> PadBounds:
         epx = ep_loc.x
@@ -108,10 +234,10 @@ class Board(device.Board):
                     reagent_id_circle_radius = 1,
                     reagent_id_circle_center = self._reagent_circle_center(exit_pad.location) 
             )
-        gate_electrode = Electrode(gate_loc.x, gate_loc.y, self._states)
+        gate_electrode = self._firmware.make_electrode(gate_loc.x, gate_loc.y)
         gate = WellGate(self, exit_pad, exit_dir, gate_electrode, neighbors=(0,1))
-        pad_neighbors = [[-1,1,2], [0,2], [0, 1]]
-        inner_electrodes = tuple(Electrode(x, y, self._states) for x,y in inner_locs)
+        pad_neighbors = [[-1,1,2], [-1,0,2], [0, 1]]
+        inner_electrodes = tuple(self._firmware.make_electrode(x, y) for x,y in inner_locs)
         shared = tuple(WellPad(self, state=s, neighbors=ns) for s,ns in zip(inner_electrodes, pad_neighbors))
         return Well(number=num,
                     board=self,
@@ -125,7 +251,7 @@ class Board(device.Board):
                     shape=shape
                     )
     
-    def __init__(self, dev : Optional[str], *,
+    def __init__(self, od_firmware : OpenDropFirmware, *,
                  off_on_delay: Time = Time.ZERO) -> None:
         pad_dict = dict[XYCoord, Pad]()
         wells: list[Well] = []
@@ -134,13 +260,11 @@ class Board(device.Board):
                          orientation=Orientation.NORTH_NEG_EAST_POS,
                          drop_motion_time=500*ms,
                          off_on_delay=off_on_delay)
-        self._dev = dev
-        self._states = bytearray(128)
-        self._port= None
+        self._firmware = od_firmware
         for x in range(1,15):
             for y in range(0,8):
                 loc = XYCoord(x, y)
-                e = Electrode(x, y, self._states)
+                e = od_firmware.make_electrode(x, y)
                 p = Pad(loc, self, e)
                 pad_dict[loc] = p
                 
@@ -158,34 +282,19 @@ class Board(device.Board):
         def inner_locs(col: int, rows: Sequence[int]) -> Sequence[tuple[int,int]]:
             return [(col, r) for r in rows]
         
-        upper_left = self._well(0, transition, Dir.RIGHT, XYCoord(0,0), self.pad_at(1,1), inner_locs(0, (1,2,3)))
-        upper_right = self._well(1, transition, Dir.LEFT, XYCoord(15,0), self.pad_at(14,1), inner_locs(15, (1,2,3)))
-        lower_left = self._well(2, transition, Dir.RIGHT, XYCoord(0,7), self.pad_at(1,6), inner_locs(0, (6,5,4)))
-        lower_right = self._well(3, transition, Dir.LEFT, XYCoord(15,7), self.pad_at(14,6), inner_locs(15, (6,5,4)))
+        upper_left = self._well(0, transition, Dir.RIGHT, XYCoord(0,0), self.pad_at(1,1), inner_locs(0, (2,1,3)))
+        upper_right = self._well(1, transition, Dir.LEFT, XYCoord(15,0), self.pad_at(14,1), inner_locs(15, (2,1,3)))
+        lower_left = self._well(2, transition, Dir.RIGHT, XYCoord(0,7), self.pad_at(1,6), inner_locs(0, (5,6,4)))
+        lower_right = self._well(3, transition, Dir.LEFT, XYCoord(15,7), self.pad_at(14,6), inner_locs(15, (5,6,4)))
         wells.extend((upper_left, upper_right, lower_left, lower_right))
         
     def update_state(self) -> None:
-        if self._port is None:
-            if self._dev is not None:
-                self._port = Serial(self._dev)
-                # self._stream = open(self._dev, "wb")
-        if self._port is not None:
-            self._port.write(self._states)
-            # I'm not sure why, but it seems that nothing happens until the 
-            # first byte of the next round gets sent. (Sending 129 bytes works, 
-            # but then the next round will use that extra byte.  Sending everything
-            # twice seems to do the job.  I'll look into this further.
-            self._port.write(self._states)
+        self._firmware.update_state(self.off_on_delay)
         super().update_state()
         
     def stop(self)->None:
-        if self._port is not None:
-            self._port.close()
-            self._port = None
+        self._firmware.stop()
         super().stop()
-        
-    # def electrode(self, i: int) -> Electrode:
-    #     return Electrode(i, self._states)
     
 class PlatformTask(PlatformChoiceTask):
     def __init__(self, name: str = "Opendrop",
@@ -199,8 +308,8 @@ class PlatformTask(PlatformChoiceTask):
                    exerciser: PlatformChoiceExerciser, # @UnusedVariable
                    pipettor: Pipettor) -> Board: # @UnusedVariable
         off_on_delay: Time = args.off_on_delay
-        port: Optional[str] = args.port
-        return Board(dev=port, off_on_delay=off_on_delay)
+        firmware = OpenDropFirmware.make_firmware(args)
+        return Board(od_firmware=firmware, off_on_delay=off_on_delay)
         
     def add_args_to(self, 
                      group: _ArgumentGroup, 
@@ -208,11 +317,7 @@ class PlatformTask(PlatformChoiceTask):
                      *,
                      exerciser: Exerciser) -> None:
         super().add_args_to(group, parser, exerciser=exerciser)
-        group.add_argument('-p', '--port',
-                           help='''
-                           The communication port (e.g., COM5) to use to talk to the board.
-                           By default, only the display is run
-                           ''')
+        OpenDropFirmware.add_args_to(group, parser)
         
         
     def available_wells(self, exerciser:Exerciser) -> Sequence[int]: # @UnusedVariable
--- mpam/src/devices/wombat.py
+++ mpam/src/devices/wombat.py
@@ -1,19 +1,18 @@
 from __future__ import annotations
 
-from enum import Enum, auto
+from argparse import Namespace, _ArgumentGroup, ArgumentParser
+
+import logging
 from typing import Mapping, Final, Optional, Sequence
 
-from serial import Serial
-
 from devices import joey
-from mpam.types import OnOff, State, DummyState
+from devices.opendrop import OpenDropFirmware, Electrode
 from erk.basic import ComputedDefaultDict
-from quantities.dimensions import Time
-import logging
 from mpam.exerciser import PlatformChoiceExerciser, Exerciser
-from argparse import Namespace, _ArgumentGroup, ArgumentParser,\
-    BooleanOptionalAction
 from mpam.pipettor import Pipettor
+from mpam.types import OnOff, State, DummyState
+from quantities.dimensions import Time
+
 
 logger = logging.getLogger(__name__)
 
@@ -81,40 +80,6 @@ _opendrop: Mapping[int, tuple[int,int]] = {
     237: (16, 1), 238: (16, 2), 239: (16, 3), 240: (16, 4),
     241: (16, 5), 242: (16, 6), 243: (16, 7), 244: (16, 8),
 }
-# _opendrop: Mapping[int, tuple[int,int]] = {
-#     244: (1,1), 243: (1,2), 242: (1,3), 241: (1,4), 
-#     240: (1,5), 239: (1,6), 238: (1,7), 237: (1,8),
-#     236: (2,1), 235: (2,2), 234: (2,3), 233: (2,4),
-#     232: (2,5), 231: (2,6), 230: (2,7), 229: (2,8),
-#     228: (3,1), 227: (3,2), 226: (3,3), 225: (3,4),
-#     224: (3,5), 223: (3,6), 222: (3,7), 221: (3,8),
-#     220: (4,1), 219: (4,2), 218: (4,3), 217: (4,4),
-#     216: (4,5), 215: (4,6), 214: (4,7), 213: (4,8),
-#     212: (5,1), 211: (5,2), 210: (5,3), 209: (5,4),
-#     208: (5,5), 207: (5,6), 206: (5,7), 205: (5,8),
-#     204: (6,1), 203: (6,2), 202: (6,3), 201: (6,4),
-#     200: (6,5), 199: (6,6), 198: (6,7), 197: (6,8),
-#     196: (7,1), 195: (7,2), 194: (7,3), 193: (7,4),
-#     192: (7,5), 191: (7,6), 190: (7,7), 189: (7,8),
-#     188: (8,1), 187: (8,2), 186: (8,3), 185: (8,4),
-#     184: (8,5), 63: (8,6), 64: (8,7), 65: (8,8),
-#     60: (9,1), 61: (9,2), 62: (9,3), 183: (9,4),
-#     182: (9,5), 181: (9,6), 180: (9,7), 179: (9,8),
-#     178: (10,1), 177: (10,2), 176: (10,3), 175: (10,4),
-#     174: (10,5), 173: (10,6), 172: (10,7), 171: (10,8),
-#     170: (11,1), 169: (11,2), 168: (11,3), 167: (11,4),
-#     166: (11,5), 165: (11,6), 164: (11,7), 163: (11,8),
-#     162: (12,1), 161: (12,2), 160: (12,3), 159: (12,4),
-#     158: (12,5), 157: (12,6), 156: (12,7), 155: (12,8),
-#     154: (13,1), 153: (13,2), 152: (13,3), 151: (13,4),
-#     150: (13,5), 149: (13,6), 148: (13,7), 147: (13,8),
-#     146: (14,1), 145: (14,2), 144: (14,3), 143: (14,4),
-#     142: (14,5), 141: (14,6), 140: (14,7), 139: (14,8),
-#     138: (15,1), 137: (15,2), 136: (15,3), 135: (15,4),
-#     134: (15,5), 133: (15,6), 132: (15,7), 131: (15,8),
-#     130: (16,1), 129: (16,2), 128: (16,3), 127: (16,4),
-#     126: (16,5), 125: (16,6), 124: (16,7), 123: (16,8),
-# }
 
 _shared_pad_cells: Mapping[tuple[str,int], str] = {
     ('left', 0): 'BC27', ('left', 1): 'B27', ('left', 2): 'AB27', 
@@ -130,57 +95,17 @@ _well_gate_cells: Mapping[int, str] = {
     4: 'T06', 5: 'N06', 6: 'H06', 7: 'B06'
     }
 
-class OpenDropVersion(Enum):
-    V40 = auto()
-    V41 = auto()
-
-class Electrode(State[OnOff]):
-    byte_index: Final[int]
-    bit_index: Final[int]
-
-    array: Final[bytearray]
-    
-    def realize_state(self, val: OnOff) -> None:
-        # assert self.index < len(self.array), f"{self.index} not in {len(self.array)}-byte array"
-        
-        bit = 1 << self.bit_index
-
-        if val is OnOff.ON:
-            self.array[self.byte_index] |= bit
-        else:
-            self.array[self.byte_index] &= ~bit
-        
-        # self.array[self.index] = 1 if val else 0
-    
-    def __init__(self, byte_index: int, bit_index: int, a: bytearray) -> None:
-        super().__init__(initial_state=OnOff.OFF)
-        self.byte_index = byte_index
-        self.bit_index = bit_index
-        self.array = a
         
 
 
 class Board(joey.Board):
-    _device: Final[Optional[str]]
-    _states: Final[bytearray]
-    _last_states: bytearray
-    _port: Optional[Serial] 
-    _od_version: Final[OpenDropVersion]
+    _firmware: Final[OpenDropFirmware]
     _electrodes: Final[dict[int, Electrode]]
-    _double_write: Final[bool]
     is_yaminon: Final[bool]
     
     def make_electrode(self, pin: int) -> Electrode:
         x,y = _opendrop[pin]
-        if self._od_version is OpenDropVersion.V40:
-            byte_index = (x-1)*8+(y-1)
-            bit_index = 0
-            assert byte_index < 128
-        else:
-            byte_index = x-1
-            bit_index = y-1
-        # print(f"  pin: {pin}, (x,y): ({x},{y}), index: {index}")
-        return Electrode(byte_index, bit_index, self._states)
+        return self._firmware.make_electrode(x-1,y-1)
     
     def _electrode(self, cell: Optional[str]) -> Optional[Electrode]:
         if cell is None:
@@ -207,62 +132,29 @@ class Board(joey.Board):
         if self.is_yaminon and y >= 12:
             y = y-12
         cell = f"{ord('B')+y:c}{25-x:02d}"
-        # print(f"({x}, {y}): {cell}")
+        if cell in _pins:
+            print(f"({x}, {y}): {cell} : {_pins[cell]} : {_opendrop[_pins[cell]]}")
+        
         return self._electrode(cell)
     
-    def __init__(self, device: Optional[str], od_version: OpenDropVersion, *,
+    def __init__(self, od_firmware: OpenDropFirmware, *,
                  is_yaminon: bool = False,
                  off_on_delay: Time = Time.ZERO,
-                 double_write: bool = True,
                  pipettor: Optional[Pipettor] = None) -> None:
-        if od_version is OpenDropVersion.V40:
-            n_state_bytes = 128
-        elif od_version is OpenDropVersion.V41:
-            n_state_bytes = 32
-        else:
-            assert False, f"Unknown OpenDrop version: {od_version}"
-        self._states = bytearray(n_state_bytes)
-        self._last_states = bytearray(n_state_bytes)
-        self._od_version = od_version
+        self._firmware = od_firmware
         self._electrodes = ComputedDefaultDict[int, Electrode](lambda pin: self.make_electrode(pin))
         logger.info("is_yaminon = %s", is_yaminon)
         self.is_yaminon = is_yaminon
-        logger.info("double_write = %s", double_write)
-        self._double_write = double_write
         super().__init__(pipettor=pipettor, off_on_delay=off_on_delay)
-        self._device = device
-        self._port = None
-        
-    def send_states(self, states: bytearray) -> None:
-        logger.debug("sending %s", states.hex())
-        if self._port is not None:
-            self._port.write(states)
-            # I'm not sure why, but it seems that nothing happens until the 
-            # first byte of the next round gets sent. (Sending 129 bytes works, 
-            # but then the next round will use that extra byte.  Sending everything
-            # twice seems to do the job.  I'll look into this further.
-            self._port.write(states)
-        
         
     def update_state(self) -> None:
-        if self._port is None and self._device is not None:
-            self._port = Serial(self._device)
-        delay = self.off_on_delay
-        new_states = bytearray(self._states)
-        send_first: Optional[bytearray] = None
-        if delay > 0:
-            # First we send 
-            send_first = bytearray(x&y for x,y in zip(self._last_states, new_states))
-        elif delay < 0:
-            send_first = bytearray(x|y for x,y in zip(self._last_states, new_states))
-            delay = -delay
-        if send_first is not None:
-            self.send_states(send_first)
-            logger.debug("Delaying for %s", delay)
-            delay.sleep()
-        self.send_states(new_states)
-        self._last_states = new_states
+        self._firmware.update_state(self.off_on_delay)
         super().update_state()
+        
+    def stop(self)->None:
+        self._firmware.stop()
+        super().stop()
+        
 
 class PlatformTask(joey.PlatformTask):
     def __init__(self, name: str = "Wombat",
@@ -276,10 +168,11 @@ class PlatformTask(joey.PlatformTask):
                    exerciser: PlatformChoiceExerciser, # @UnusedVariable
                    pipettor: Pipettor) -> Board: # @UnusedVariable
         logger.info(f"Version is {args.od_version}")
+        firmware = OpenDropFirmware.make_firmware(args)
         return Board(pipettor=pipettor,
-                     device=args.port, od_version=args.od_version, is_yaminon=self.is_yaminon(),
-                     off_on_delay=args.off_on_delay,
-                     double_write=args.double_write)
+                     od_firmware=firmware,
+                     is_yaminon=self.is_yaminon(),
+                     off_on_delay=args.off_on_delay)
         
     def is_yaminon(self) -> bool:
         return False
@@ -293,25 +186,7 @@ class PlatformTask(joey.PlatformTask):
                     *,
                     exerciser: Exerciser) -> None:
         super().add_args_to(group, parser, exerciser=exerciser)
-        group.add_argument('-p', '--port',
-                           help='''
-                           The communication port (e.g., COM5) to use to talk to the board.
-                           By default, only the display is run
-                           ''')
-        vg = group.add_mutually_exclusive_group()
-        vg.add_argument('-4.0', action='store_const', const=OpenDropVersion.V40, dest='od_version',
-                        help="The OpenDrop board uses firmware version 4.0")
-        vg.add_argument('-4.1', action='store_const', const=OpenDropVersion.V41, dest='od_version',
-                        help="The OpenDrop board uses firmware version 4.1")
-        # group.add_argument('--yaminon', action='store_true',
-        #                    help="Mirror pads on top and bottom of the board.")
-        double_write_default = True
-        group.add_argument('--double-write', action=BooleanOptionalAction, default=double_write_default,
-                           help=f'''
-                           Send state array to OpenDrop twice.  
-                           Default is {double_write_default}
-                           ''')
-        parser.set_defaults(od_version=OpenDropVersion.V40)
+        OpenDropFirmware.add_args_to(group, parser)
         
 class YaminonPlatformTask(PlatformTask):
     def __init__(self, name: str = "Yaminon",
